e22d15b3aa93f05d50ceadad18c97b03266fbc52
rfoo1 <rfoo1@asu.edu>
2025-06-13 03:35:21 -0700

    Navigation buttons update
    Overhaul w nav-btn (instead of btn-primary)

---
+++ nav_control.js
@@ -0,0 +1,12 @@
+// File name: nav_controls.js
+// File description: navigation control active state change
+
+const links = document.querySelectorAll('.controls a');
+const currentPath = window.location.pathname.split('/').pop();
+
+links.forEach(link => {
+if (link.getAttribute('href') === currentPath) {
+    link.classList.add('active');
+    // link.setAttribute.diss
+}
+});
